b8448e69845d8933ecccc9aa88f5081eed7cfbec
Serge3006 <chavezferia.justinian@gmail.com>
2020-05-16 22:12:10 +0200

    unit tests concepts, ellipses

--- tests/test_finding.py
+++ tests/test_finding.py
@@ -5,17 +5,81 @@ sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
 from modules.finding import *
 from modules.utils import read_drawio_xml
 
-class TestConcepts(unittest.TestCase):
-    def test_concept_structure(self):
-        """
-        Test that the structure of the concept found is correct
-        """
-        data = read_drawio_xml("tests/inputs/test_concept_structure.xml")
-        concepts, _ = find_concepts_and_attributes(data)
+class TestFindingFunctions(unittest.TestCase):
+
+    def test_concepts(self):
+
+        test = read_drawio_xml("tests/inputs/test_concepts.xml")
+        concepts, _ = find_concepts_and_attributes(test)
         for id, concept in concepts.items():
             self.assertEqual(id, "2")
             self.assertEqual(concept["prefix"], "ns")
             self.assertEqual(concept["uri"], "Class")
+
+    def test_unnamed(self):
+
+        test = read_drawio_xml("tests/inputs/test_unnamed.xml")
+        concepts, _ = find_concepts_and_attributes(test)
+        for id, concept in concepts.items():
+            self.assertEqual(concept["prefix"], "")
+            self.assertEqual(concept["uri"], "")
+
+    def test_namespaces(self):
+
+        test = read_drawio_xml("tests/inputs/test_namespaces.xml")
+        namespaces = find_namespaces(test)
+        for prefix, uri in namespaces.items():
+            self.assertEqual(prefix in ["base", "saref"], True)
+            self.assertEqual(uri in ["http://theOntology.namespace.com", "http://saref.com"], True)
+
+    def test_intersection_1(self):
+
+        test = read_drawio_xml("tests/inputs/test_intersection_1.xml")
+        ellipses = find_ellipses(test)
+        for id, ellipse in ellipses.items():
+            self.assertEqual(ellipse["type"], "owl:intersectionOf")
+            self.assertEqual(ellipse["group"], ["3", "4"])
+
+    def test_intersection_2(self):
+
+        test = read_drawio_xml("tests/inputs/test_intersection_2.xml")
+        ellipses = find_ellipses(test)
+        for id, ellipse in ellipses.items():
+            self.assertEqual(ellipse["type"], "owl:intersectionOf")
+            self.assertEqual(ellipse["group"], ["3", "4"])
+
+    def test_union_1(self):
+
+        test = read_drawio_xml("tests/inputs/test_union_1.xml")
+        ellipses = find_ellipses(test)
+        for id, ellipse in ellipses.items():
+            self.assertEqual(ellipse["type"], "owl:unionOf")
+            self.assertEqual(ellipse["group"], ["3", "4"])
+
+    def test_union_2(self):
+
+        test = read_drawio_xml("tests/inputs/test_union_2.xml")
+        ellipses = find_ellipses(test)
+        for id, ellipse in ellipses.items():
+            self.assertEqual(ellipse["type"], "owl:unionOf")
+            self.assertEqual(ellipse["group"], ["3", "4"])
+
+    def test_equivalent_1(self):
+
+        test = read_drawio_xml("tests/inputs/test_equivalent_1.xml")
+        ellipses = find_ellipses(test)
+        for id, ellipse in ellipses.items():
+            self.assertEqual(ellipse["type"], "owl:equivalentClass")
+            self.assertEqual(ellipse["group"], ["3", "4"])
+
+    def test_disjoint_1(self):
+
+        test = read_drawio_xml("tests/inputs/test_disjoint_1.xml")
+        ellipses = find_ellipses(test)
+        for id, ellipse in ellipses.items():
+            self.assertEqual(ellipse["type"], "owl:disjointWith")
+            self.assertEqual(ellipse["group"], ["3", "4"])
+
 
 if __name__ == "__main__":
     unittest.main()
