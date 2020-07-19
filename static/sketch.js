dff317e1a31bc74936609e98228d0d9a5f96b72a
Serge3006 <chavezferia.justinian@gmail.com>
2020-07-20 01:17:45 +0200

    code seen after transformation

--- static/sketch.js
+++
@@ -1,8 +0,0 @@
-function changeFileName() {
-
-    var theFile = document.getElementById("files");
-    var theText = document.getElementById("upload-text");
-    fileName = this.value
-    theText.innerHTML(fileName)
-
-}
