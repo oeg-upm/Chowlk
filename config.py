b88aa331c03907c1cd2c4a9d9b10c592412b565a
Serge3006 <chavezferia.justinian@gmail.com>
2022-06-29 23:04:47 +0200

    eliminate unnecesary files and folders

--- config.py
+++ config.py
@@ -1,10 +1,9 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "hard to guess string"
-    TEMPORAL_FOLDER = os.environ.get("TEMPORAL_FOLDER") or "tmp"
+    SECRET_KEY = os.getenv("SECRET_KEY") or "hard to guess string"
+    TEMPORAL_FOLDER = os.getenv("TEMPORAL_FOLDER") or "tmp"
     TEMPLATES_AUTORELOAD = True
-    SQLALCHEMY_DATABASE_URI = "postgresql:///chowlk"
 
 class DevelopmentConfig(Config):
     DEBUG = True
