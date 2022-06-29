c609fe24292fba26b95559f12b0fc93620d01073
Serge Chavez <esx0004371@prosegur.com>
2022-06-29 22:34:41 +0200

    fix: remove database

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,14 @@
 FROM python:3.7
 
-WORKDIR /usr/src/app
+WORKDIR /app
 
 COPY . .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-
 EXPOSE 5000
 
-CMD ["python", "./app.py"]
+#CMD ["python", "./app.py"]
 
 
 
--- app.py
+++ app.py
@@ -22,30 +22,8 @@ config_name = os.environ.get("APP_MODE") or "development"
 app = flask.Flask(__name__)
 app.config.from_object(config[config_name])
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
 CORS(app)
 bootstrap = Bootstrap(app)
-
-# Schema definition
-class User(db.Model):
-
-    __tablename__ = "users"
-
-    id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.DateTime)
-    input = db.Column(db.String)
-    output = db.Column(db.String)
-    errors = db.Column(JSON)
-
-    def __init__(self, date, input, output, errors):
-        self.date = date
-        self.input = input
-        self.output = output
-        self.errors = errors
-
-    def __repr__(self) -> str:
-        return super().__repr__()
-
 
 @app.route("/static/<path:path>")
 def send_static(path):
@@ -67,83 +45,6 @@ app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 @app.route("/", methods=["GET", "POST"])
 def home():
     return render_template("home.html")
-
-"""@app.route("/demoeswc2021", methods=["GET", "POST"])
-def poster():
-    return render_template("chowlk_poster.html")"""
-
-
-"""@app.route("/download/<path:format>", methods=["GET", "POST"])
-def download(format):
-    ontology_directory = os.path.join(current_app.root_path, app.config["TEMPORAL_FOLDER"])
-    if format == "ttl":
-        return send_from_directory(ontology_directory, session.get("ttl_filename"), as_attachment=True)
-    elif format == "xml":
-        return send_from_directory(ontology_directory, session.get("xml_filename"), as_attachment=True)"""
-
-
-"""@app.route("/diagram_upload", methods=["GET", "POST"])
-def diagram_upload():
-
-    if request.method == "POST" and "diagram_data" in request.files:
-        file = request.files["diagram_data"]
-        
-        filename = file.filename
-
-        # Verifying that there exists a file
-        if filename == "":
-            error = "No file choosen. Please choose a diagram."
-            flash(error)
-            return redirect(url_for("home"))
-
-        os.makedirs("data", exist_ok=True)
-        input_path = os.path.join("data", filename)
-
-        # Creating the dirs to then store the output
-        ttl_filename = filename[:-3] + "ttl"
-        xml_filename = filename[:-3] + "owl"
-
-        if not os.path.exists(app.config["TEMPORAL_FOLDER"]):
-            os.makedirs(app.config["TEMPORAL_FOLDER"])
-        
-        ttl_filepath = os.path.join(app.config["TEMPORAL_FOLDER"], ttl_filename)
-        xml_filepath = os.path.join(app.config["TEMPORAL_FOLDER"], xml_filename)
-
-        # Reading and transforming the diagram
-        root = read_drawio_xml(file)
-        turtle_file_string, xml_file_string, new_namespaces, errors = transform_ontology(root)
-
-        # Eliminating keys that do not contain errors
-        new_errors = copy.copy(errors)
-        for key, error in errors.items():
-            if len(error) == 0:
-                del new_errors[key]
-
-        # Storing the output in ttl and xml
-        with open(ttl_filepath, "w") as file:
-            file.write(turtle_file_string)
-
-        with open(xml_filepath, "w") as file:
-            file.write(xml_file_string)
-
-        session["ttl_filename"] = ttl_filename
-        session["xml_filename"] = xml_filename
-
-
-        with open(ttl_filepath, "r", encoding='utf-8') as f:
-            ttl_data = f.read()
-            ttl_data = ttl_data.split('\n')
-
-        with open(xml_filepath, "r", encoding='utf-8') as f:
-            xml_data = f.read()
-            xml_data = xml_data.split('\n')
-
-        user = User(datetime.now(), input_path, ttl_filepath, errors)
-        db.session.add(user)
-        db.session.commit()
-
-        return render_template("output.html", ttl_data=ttl_data, xml_data=xml_data, namespaces=new_namespaces, errors=new_errors)"""
-
 
 @app.route("/api", methods=["GET", "POST"])
 def api():
@@ -184,10 +85,6 @@ def api():
 
         with open(ttl_filepath, "r") as f:
             ttl_data = f.read()
-
-        user = User(datetime.now(), input_path, ttl_filepath, errors)
-        db.session.add(user)
-        db.session.commit()
 
         return {"ttl_data": ttl_data, "errors": new_errors, "new_namespaces": new_namespaces}
 
