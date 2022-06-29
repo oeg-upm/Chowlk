e00b7b2e695b4dda5b17b504a646d2e4542d4d12
Serge3006 <chavezferia.justinian@gmail.com>
2022-06-30 00:51:00 +0200

    feat: avoid save of temporal files

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import flask
 import copy
 from flask import request, url_for, render_template, redirect, flash, \
                     send_from_directory, session, jsonify
-from flask_bootstrap import Bootstrap
 from flask_cors import CORS
 
 from source.chowlk.transformations import transform_ontology
@@ -12,43 +11,17 @@ import xml.etree.ElementTree as ET
 from config import config
 
 
-config_name = os.environ.get("APP_MODE") or "development"
+config_name = os.getenv("APP_MODE", "development")
 
 app = flask.Flask(__name__)
 app.config.from_object(config[config_name])
 CORS(app)
-bootstrap = Bootstrap(app)
-
-@app.route("/static/<path:path>")
-def send_static(path):
-    return send_from_directory("static", path)
-
-
-@app.route("/", methods=["GET", "POST"])
-def home():
-    return render_template("home.html")
 
 @app.route("/api", methods=["GET", "POST"])
 def api():
 
     if request.method == "POST":
         file = request.files["data"]
-        filename = file.filename
-
-        if filename == "":
-            error = "No file choosen. Please choose a diagram."
-            flash(error)
-            return redirect(url_for("home"))
-
-        os.makedirs("data", exist_ok=True)
-        input_path = os.path.join("data", filename)
-
-        ttl_filename = filename[:-3] + "ttl"
-
-        if not os.path.exists(app.config["TEMPORAL_FOLDER"]):
-            os.makedirs(app.config["TEMPORAL_FOLDER"])
-
-        ttl_filepath = os.path.join(app.config["TEMPORAL_FOLDER"], ttl_filename)
 
         # Reading and transforming the diagram
         root = read_drawio_xml(file)
@@ -60,15 +33,7 @@ def api():
             if len(error) == 0:
                 del new_errors[key]
 
-        with open(ttl_filepath, "w") as file:
-            file.write(turtle_file_string)
-
-        session["ttl_filename"] = ttl_filename
-
-        with open(ttl_filepath, "r") as f:
-            ttl_data = f.read()
-
-        return {"ttl_data": ttl_data, "errors": new_errors, "new_namespaces": new_namespaces}
+        return {"ttl_data": turtle_file_string, "errors": new_errors, "new_namespaces": new_namespaces}
 
 
 @app.errorhandler(500)
@@ -76,5 +41,5 @@ def handle_500_error(e):
     return jsonify({"error": "Server error, review the input diagram"}), 500
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.getenv("PORT", 5000))
     app.run(host="0.0.0.0", port=port, debug=app.config["DEBUG"])
--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or "hard to guess string"
+    SECRET_KEY = os.getenv("SECRET_KEY", "hard to guess string")
     TEMPORAL_FOLDER = os.environ.get("TEMPORAL_FOLDER") or "tmp"
-    TEMPLATES_AUTORELOAD = True
 
 class DevelopmentConfig(Config):
     DEBUG = True
